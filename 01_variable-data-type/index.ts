09493dcca0c1e6046b6c0e88fef731a1e33ae6a5
Naufal Afif <naufalafif58@gmail.com>
2019-08-19 18:25:12 +0700

    init variable & data type

---
+++ 01_variable-data-type/index.ts
@@ -0,0 +1,25 @@
+// 
+
+// Primittive
+let nama:String; // Menerima Text
+let tinggiBadan:Number; // Menerima Angka Integer, Float, Double, Hexa, Octal, Biner
+let menikah:Boolean; // Menerima Value True or False 
+let catatan:any; //Menerima Semua Masukan
+
+// ENUM
+enum Saklar {
+  HIDUP, MATI
+}
+
+console.log(Saklar.HIDUP); // 0
+console.log(Saklar.MATI); // 1
+
+
+// ARRAY
+let namaPeserta:String[] = ["Budi", "Andi", "Siti"]; // Array of String
+let jumlahOmsetPerBulan:Number[] = [13232, 421323, 42333]; // Array of Number
+
+let namaBuah:Array<String> = ["Apel", "Jambu", "Semangka"]
+let jumlahLabaPerBulan:Array<Number> = [11111, 22222, 33333]
+
+
